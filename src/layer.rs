b30528e0619658716eaf4015322e52342f970e45
rice7th <rice7th@gmail.com>
2024-09-30 01:25:18 +0200

    [Renderer] Reworked shaders.

--- src/layer.rs
+++ src/layer.rs
@@ -5,34 +5,55 @@ use rgb::Rgba;
 /// # Layer
 /// A Layer is a simple structure that holds
 /// a buffer that contains a list of coverage
-/// values for each pixel, and a function
-/// `fill` that given four mandatory parameters
-/// (x, y of a pixel and width and height of
-/// the Layer) and a custom parameter C, containing
-/// any other data that the user may want to
-/// provide (e.g. gradient stops, noise seeds,
-/// textures etc.) runs for each pixel filling
-/// it in with some color.
+/// values for each pixel, and a material `M`,
+/// responsible for painting each pixel.
+pub struct Layer<M> 
+where
+    M: Shader
+{
+    size: Vec2,
+    coverage: Vec<f32>,
+    material: M,
+}
+
+impl<M> Layer<M> where M: Shader {
+    pub fn paint(&self) -> Rgba<f32> {
+        todo!()
+    }
+}
+
+// DUH
+/// # Shader
+/// A simple interface for designing custom
+/// painting operations.
 /// 
 /// It is analog to fragment shaders in OpenGL
 /// or Vulkan, but on CPU. This allows for a
 /// limitless amount of flexibility when choosing
 /// any fill option, instead of sticking to the
 /// classic Solid-Gradient-Texture format.
-pub struct Layer<F, C> 
-where
-    F: Fn(Vec4, C) -> Rgba<f32>
-{
-    size: Vec2,
-    coverage: Vec<f32>,
-    material: F,
-    _marker: std::marker::PhantomData<fn(C)>,
-}
-
-impl<F, C> Layer<F, C> where F: Fn(Vec4, C) -> Rgba<f32> {
-    pub fn fill(&self) -> Rgba<f32> {
-        // TODO: How do I even approach this problem?
-        //(self.material)(x, y, w, h, )
-        todo!()
-    }
+/// 
+/// For example, a simple Circle SDF can be easily
+/// done like this:
+/// ```rust
+/// pub struct Circle {
+///     radius: f32,
+///     color: Rgba<f32>
+/// }
+/// 
+/// impl Shader for Circle {
+///     fn fill(&self, x: f32, y: f32, w: f32, h: f32) -> Rgba<f32> {
+///         if f32::hypot(x, y) < self.radius {
+///             return self.color;
+///         } else {
+///             return Rgba::from([0.0, 0.0, 0.0, 0.0]);
+///         }        
+///     }
+/// }
+/// ```
+/// The fields of the struct are analogous to OpenGL's
+/// `uniforms` or attributes and can of course store any
+/// type of data.
+pub trait Shader {
+    fn fill(&self, _x: f32, _y: f32, _w: f32, _h: f32) -> Rgba<f32>;
 }
